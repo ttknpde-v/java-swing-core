f806709a128ae3806f031d942c0ff151f22bfc86
ttknpde-v <thitikorn_nupan@outlook.co.th>
2023-11-21 16:33:41 +0700

    updated

--- src/ttknpdev/understandui/SixthSwing.java
+++ src/ttknpdev/understandui/SixthSwing.java
@@ -17,7 +17,7 @@ import ttknpdev.entities.StudentTableModel;
 import ttknpdev.frames.MyFrames;
 import ttknpdev.log.MyLog;
 
-public class SixthSwing implements ActionListener , ListSelectionListener {
+public class SixthSwing implements ActionListener, ListSelectionListener {
 
    // understand Jtable 
    private final MyFrames frame;
@@ -25,7 +25,11 @@ public class SixthSwing implements ActionListener , ListSelectionListener {
    private JScrollPane jScrollPane;
    private StudentTableModel studentTableModel;
    private List<Student> students;
+
    public SixthSwing() {
+      jTable = new JTable();
+      jScrollPane = new JScrollPane();
+      studentTableModel = new StudentTableModel(null);
       frame = new MyFrames("testing Jtable component");
    }
 
@@ -40,52 +44,32 @@ public class SixthSwing implements ActionListener , ListSelectionListener {
       String column[] = {"ID", "NAME", "SALARY"};
 
       jTable = new JTable(data, column);
-      
+
       jScrollPane = new JScrollPane(jTable);
 
       frame.add(jScrollPane);
       frame.setResizable(true);
       frame.setSize(400, 400);
    }
+
    // for entity 1
    private void understandJTableWithEntity1() {
-      Student s1 = new Student("Peter Parker", (short) 23, (short) 4, 2.6F);
-      Student s2 = new Student("Adam Ryder", (short) 20, (short) 3, 3.1F);
-      students = new ArrayList<>(); // key!!
-      students.add(s1);
-      students.add(s2);
-      studentTableModel = new StudentTableModel(students); 
-      // studentTableModel.fireTableDataChanged();      
-
-      // jTable = new JTable(studentTableModel);
-      // *** 
-      // students = (List<Student>) studentTableModel;
-      
-      jTable = new JTable();
-      
-      
+      frame.setVisible(true);
       jTable.setModel(studentTableModel);
-      // jTable.setAutoCreateColumnsFromModel(true);
-      // jTable.revalidate();
-
-            // jTable.repaint();
-      jScrollPane = new JScrollPane(jTable);
-      
-      
+      jScrollPane.getViewport().add(jTable);
       frame.add(jScrollPane);
-      // frame.setLayout(null);
-      // frame.add(jTable);
       frame.setResizable(true);
       frame.setSize(400, 400);
- 
       
       JButton jButtonRemove = new JButton("remove last student");
       jButtonRemove.setBounds(0, 500, 60, 60);
       // frame.setLayout(new GridLayout());
       frame.add(jButtonRemove, BorderLayout.SOUTH);
-
-      
       jButtonRemove.addActionListener(this);
+   }
+
+   public void reset() {
+       jScrollPane.getViewport().add(jTable);
    }
 
    @Override
@@ -95,36 +79,36 @@ public class SixthSwing implements ActionListener , ListSelectionListener {
       System.out.println("clicked");
       // students.remove(students.size()-1); work!
       studentTableModel.removeRow();
+      // understandJTableWithEntity1();
+      reset();
    }
-   
+
    // for entity 2
-   
    public void understandJTableWithEntity2() {
       Student s1 = new Student("Peter Parker", (short) 23, (short) 4, 2.6F);
       Student s2 = new Student("Adam Ryder", (short) 20, (short) 3, 3.1F);
-      students = new ArrayList<>(); 
+      students = new ArrayList<>();
       students.add(s1);
       students.add(s2);
-      studentTableModel = new StudentTableModel(students); 
-      
+      studentTableModel = new StudentTableModel(students);
+
       jTable = new JTable();
       jTable.setModel(studentTableModel);
       jTable.setCellSelectionEnabled(true); // can selected once field
-      
+
       ListSelectionModel selectionModel;
       selectionModel = jTable.getSelectionModel();
       selectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
       selectionModel.addListSelectionListener(this::valueChanged);
-      
-      
+
       jScrollPane = new JScrollPane(jTable);
       //jScrollPane.setSize(300, 100);
-      jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
-      jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);  
-        
+      jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+      jScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
       //jScrollPane.setBounds(100,100, 50,100);
       frame.setResizable(true);
-      frame.setSize(300,300);
+      frame.setSize(300, 300);
       frame.add(jScrollPane);
    }
 
@@ -133,20 +117,19 @@ public class SixthSwing implements ActionListener , ListSelectionListener {
       // throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
       MyLog.log.warn("runed");
       Object data = null;
-      int []row = jTable.getSelectedRows();
-      int []columns = jTable.getSelectedColumns();
-       for (int i = 0; i < row.length; i++) {  
-          MyLog.log.info("first loop for row lenght ");
-          for (int j = 0; j < columns.length; j++) {
-             MyLog.log.info("second loop for columns lenght ");
-             data =  jTable.getValueAt(row[i], columns[j]);  
-          }
-       } 
-       MyLog.log.info("element "+data+"selected");
+      int[] row = jTable.getSelectedRows();
+      int[] columns = jTable.getSelectedColumns();
+      for (int i = 0; i < row.length; i++) {
+         MyLog.log.info("first loop for row lenght ");
+         for (int j = 0; j < columns.length; j++) {
+            MyLog.log.info("second loop for columns lenght ");
+            data = jTable.getValueAt(row[i], columns[j]);
+         }
+      }
+      MyLog.log.info("element " + data + "selected");
    }
-   
-   
+
    public static void main(String[] args) {
-      new SixthSwing().understandJTableWithEntity2();
+      new SixthSwing().understandJTableWithEntity1();
    }
 }
